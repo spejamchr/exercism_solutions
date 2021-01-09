29c3c799ff79db48e9dcf988c6578aff00e1c55d
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 14:44:08 -0700

    complete: High Scores on Rust track

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        HighScores {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        rev_sort(&self.scores).iter().take(3).copied().collect()
+    }
+}
+
+fn rev_sort(v: &[u32]) -> Vec<u32> {
+    let mut c = v.to_owned();
+    c.sort();
+    c.reverse();
+    c
+}
