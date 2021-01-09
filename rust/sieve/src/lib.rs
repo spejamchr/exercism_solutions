0865872156b9c66cc4345ac3183c72376df361e6
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 15:00:53 -0700

    revise: Sieve on Rust track

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -3,12 +3,12 @@ pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut primes: Vec<u64> = Vec::new();
     while let [n, ..] = possibles[..] {
         primes.push(n);
-        let mut i = 1;
-        while i * n <= upper_bound {
-            if let Some(index) = possibles.iter().position(|a| *a == i * n) {
+        let mut target = n;
+        while target <= upper_bound {
+            if let Some(index) = possibles.iter().position(|a| *a == target) {
                 possibles.remove(index);
             }
-            i += 1;
+            target += n;
         }
     }
     primes
