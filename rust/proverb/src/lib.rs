d5375dbced44b9f50dfcf4fc56723f0b29a6ec73
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 14:40:26 -0700

    revise: Proverb on Rust track

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,24 +1,14 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return "".to_owned();
-    }
-
-    let mut proverb = proverb_body(list);
-    proverb.push(proverb_tail(list));
-    proverb.join("\n")
-}
-
-fn proverb_tail(list: &[&str]) -> String {
-    format!("And all for the want of a {}.", list[0])
-}
-
-fn proverb_body(list: &[&str]) -> Vec<String> {
-    list.iter()
-        .zip(list[1..].iter())
+    list.windows(2)
         .map(proverb_line)
+        .chain(list.get(0).map(proverb_tail))
         .collect()
 }
 
-fn proverb_line((a, b): (&&str, &&str)) -> String {
-    format!("For want of a {} the {} was lost.", a, b)
+fn proverb_line(c: &[&str]) -> String {
+    format!("For want of a {} the {} was lost.\n", c[0], c[1])
+}
+
+fn proverb_tail(item: &&str) -> String {
+    format!("And all for the want of a {}.", item)
 }
