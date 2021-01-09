3d98dfa6b991b985af0cba9d8d1493795e2b15b3
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 14:58:45 -0700

    revise: ETL on Rust track

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,10 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    h.iter().fold(BTreeMap::new(), |mut b, (k, v)| {
-        v.iter().map(|c| c.to_ascii_lowercase()).for_each(|c| {
-            b.insert(c, *k);
-        });
-        b
-    })
+    h.iter()
+        .flat_map(|(k, v)| v.iter().map(move |c| (c.to_ascii_lowercase(), *k)))
+        .collect()
 }
