a3488546030481c415f5af18b3c81ea2b7db71cd
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 15:05:40 -0700

    complete: Queen Attack on Rust track

---
+++ rust/queen-attack/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
+
+#[derive(Debug)]
+pub struct Queen {
+    position: ChessPosition,
+}
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(Self { rank, file }),
+            _ => None,
+        }
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Self { position }
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        let a = &self.position;
+        let b = &other.position;
+
+        a.file == b.file
+            || a.rank == b.rank
+            || a.rank - a.file == b.rank - b.file
+            || a.rank + a.file == b.rank + b.file
+    }
+}
