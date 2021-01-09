eab6e1793ab76a3a48f4009222bcf66ce9673a47
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 14:38:42 -0700

    revise: Beer Song on Rust track

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -16,12 +16,10 @@ pub fn sing(start: u32, end: u32) -> String {
 }
 
 fn n_bottles(n: u32) -> String {
-    if n == 0 {
-        "No more bottles of beer".to_owned()
-    } else if n == 1 {
-        "1 bottle of beer".to_owned()
-    } else {
-        format!("{} bottles of beer", n)
+    match n {
+        0 => "No more bottles of beer".to_owned(),
+        1 => "1 bottle of beer".to_owned(),
+        n => format!("{} bottles of beer", n),
     }
 }
 
@@ -30,24 +28,22 @@ fn n_bottles_on_the_wall(n: u32) -> String {
 }
 
 fn it_or_one(n: u32) -> String {
-    if n == 1 {
-        "it".to_owned()
-    } else {
-        "one".to_owned()
+    match n {
+        1 => "it".to_owned(),
+        _ => "one".to_owned(),
     }
 }
 
 fn second_verse(n: u32) -> String {
-    if n == 0 {
-        format!(
+    match n {
+        0 => format!(
             "Go to the store and buy some more, {}",
             n_bottles_on_the_wall(99)
-        )
-    } else {
-        format!(
+        ),
+        n => format!(
             "Take {} down and pass it around, {}",
             it_or_one(n),
             n_bottles_on_the_wall(n - 1).to_lowercase()
-        )
+        ),
     }
 }
