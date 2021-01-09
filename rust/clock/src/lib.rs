0b954f45b729475ac59e302b9100a85821fecbf7
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-09 14:45:45 -0700

    revise: Clock on Rust track

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,33 +1,32 @@
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
-    minutes: i32,
-    hours: i32,
-}
-
-fn modulo(a: i32, b: i32) -> i32 {
-    ((a % b) + b) % b
+    day_minute: i32,
 }
 
 impl Clock {
-    pub fn new(raw_hours: i32, raw_minutes: i32) -> Self {
-        let minutes = modulo(raw_minutes, 60);
-        let hours = modulo(raw_hours + (raw_minutes as f32 / 60.0).floor() as i32, 24);
-        Self { minutes, hours }
+    const HOUR_MINUTES: i32 = 60;
+    const DAY_MINUTES: i32 = Self::HOUR_MINUTES * 24;
+
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let day_minute = (minutes + hours * Self::HOUR_MINUTES).rem_euclid(Self::DAY_MINUTES);
+        Self { day_minute }
     }
 
-    pub fn add_minutes(&self, raw_minutes: i32) -> Self {
-        Self::new(self.hours, self.minutes + raw_minutes)
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Self::new(0, self.day_minute + minutes)
+    }
+
+    pub fn hour(&self) -> i32 {
+        self.day_minute.div_euclid(Self::HOUR_MINUTES)
+    }
+
+    pub fn minute(&self) -> i32 {
+        self.day_minute.rem_euclid(Self::HOUR_MINUTES)
     }
 }
 
 impl std::fmt::Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
-    }
-}
-
-impl std::cmp::PartialEq for Clock {
-    fn eq(&self, o: &Self) -> bool {
-        self.minutes == o.minutes && self.hours == o.hours
+        write!(f, "{:02}:{:02}", self.hour(), self.minute())
     }
 }
