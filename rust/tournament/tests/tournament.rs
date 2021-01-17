07b0c35a9e6ff20ce8cb7da9b631a58cfccc298b
Spencer Christiansen <jc.spencer92@gmail.com>
2021-01-16 17:38:43 -0700

    testing: Make debugging Tournament on Rust easier

--- rust/tournament/tests/tournament.rs
+++ rust/tournament/tests/tournament.rs
@@ -3,11 +3,14 @@ fn just_the_header_if_no_input() {
     let input = "";
     let expected = "Team                           | MP |  W |  D |  L |  P";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn a_win_is_three_points_a_loss_is_zero_points() {
     let input = "Allegoric Alaskans;Blithering Badgers;win";
     let expected = "".to_string()
@@ -15,11 +18,14 @@ fn a_win_is_three_points_a_loss_is_zero_points() {
         + "Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n"
         + "Blithering Badgers             |  1 |  0 |  0 |  1 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn a_win_can_also_be_expressed_as_a_loss() {
     let input = "Blithering Badgers;Allegoric Alaskans;loss";
     let expected = "".to_string()
@@ -27,11 +33,14 @@ fn a_win_can_also_be_expressed_as_a_loss() {
         + "Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3\n"
         + "Blithering Badgers             |  1 |  0 |  0 |  1 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn a_different_team_can_win() {
     let input = "Blithering Badgers;Allegoric Alaskans;win";
     let expected = "".to_string()
@@ -39,11 +48,14 @@ fn a_different_team_can_win() {
         + "Blithering Badgers             |  1 |  1 |  0 |  0 |  3\n"
         + "Allegoric Alaskans             |  1 |  0 |  0 |  1 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_one_match() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -52,11 +64,14 @@ fn there_can_be_more_than_one_match() {
         + "Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6\n"
         + "Blithering Badgers             |  2 |  0 |  0 |  2 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn a_draw_is_one_point_each() {
     let input = "Allegoric Alaskans;Blithering Badgers;draw\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -65,11 +80,14 @@ fn a_draw_is_one_point_each() {
         + "Allegoric Alaskans             |  2 |  1 |  1 |  0 |  4\n"
         + "Blithering Badgers             |  2 |  0 |  1 |  1 |  1";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_one_winner() {
     let input = "Allegoric Alaskans;Blithering Badgers;loss\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win";
@@ -78,11 +96,14 @@ fn there_can_be_more_than_one_winner() {
         + "Allegoric Alaskans             |  2 |  1 |  0 |  1 |  3\n"
         + "Blithering Badgers             |  2 |  1 |  0 |  1 |  3";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn there_can_be_more_than_two_teams() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Blithering Badgers;Courageous Californians;win\n"
@@ -93,11 +114,14 @@ fn there_can_be_more_than_two_teams() {
         + "Blithering Badgers             |  2 |  1 |  0 |  1 |  3\n"
         + "Courageous Californians        |  2 |  0 |  0 |  2 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn typical_input() {
     let input = "Allegoric Alaskans;Blithering Badgers;win\n".to_string()
         + "Devastating Donkeys;Courageous Californians;draw\n"
@@ -112,11 +136,14 @@ fn typical_input() {
         + "Blithering Badgers             |  3 |  1 |  0 |  2 |  3\n"
         + "Courageous Californians        |  3 |  0 |  1 |  2 |  1";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn incomplete_competition_not_all_pairs_have_played() {
     let input = "Allegoric Alaskans;Blithering Badgers;loss\n".to_string()
         + "Devastating Donkeys;Allegoric Alaskans;loss\n"
@@ -129,11 +156,14 @@ fn incomplete_competition_not_all_pairs_have_played() {
         + "Courageous Californians        |  2 |  0 |  1 |  1 |  1\n"
         + "Devastating Donkeys            |  1 |  0 |  0 |  1 |  0";
 
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
+
     assert_eq!(tournament::tally(&input), expected);
 }
 
 #[test]
-#[ignore]
 fn ties_broken_alphabetically() {
     let input = "Courageous Californians;Devastating Donkeys;win\n".to_string()
         + "Allegoric Alaskans;Blithering Badgers;win\n"
@@ -147,6 +177,10 @@ fn ties_broken_alphabetically() {
         + "Courageous Californians        |  3 |  2 |  1 |  0 |  7\n"
         + "Blithering Badgers             |  3 |  0 |  1 |  2 |  1\n"
         + "Devastating Donkeys            |  3 |  0 |  1 |  2 |  1";
+
+    println!("\n{}\n", input);
+    println!("{}\n", expected);
+    println!("{}\n", tournament::tally(&input));
 
     assert_eq!(tournament::tally(&input), expected);
 }
